120885403d648fe5d7653b1dcdbe6ffc5312bf07
songseulbi05-dotcom <songseulbi05@gmail.com>
2026-02-10 17:10:33 +0900

    web-dev-ai-2026

--- practice09-event.js
+++ practice09-event.js
@@ -1,6 +1,6 @@
 const imgs = document.querySelectorAll(".container img");
 const clickBtn = document.querySelector("#clickBtn");
-const restartBtn = document.querySelector("#restartBtn");
+const restartBtn = document.querySelector("#restarBtn");
 const result = document.querySelector("#result");
 
 const imgArr = ["./essets/spy1.jpg", "./essets/spy2.jpg", "./essets/spy3.jpg"];
@@ -16,11 +16,13 @@ clickBtn.addEventListener("click", () => {
 
   if (values[0] === values[1] && values[1] === values[2]) {
     result.textContent = "축하합니다!다시 시작하려면 재시작 버튼을 눌러주세요!";
+    clickBtn.setAttribute("disabled", "disabled");
+    clickBtn.setAttribute("disabled", "true");
   } else {
     result.textContent = "";
   }
 });
 
 restartBtn.addEventListener("click", () => {
-  result.textContent = "";
+  location.reload();
 });
